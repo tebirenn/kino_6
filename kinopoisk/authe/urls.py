1ce2eb913b5a642d61a8b45d3cd3ef980a700d2e
Alan <a_saudabekov@kbtu.kz>
2023-11-11 14:16:02 +0600

    movie creation

--- kinopoisk/authe/urls.py
+++ kinopoisk/authe/urls.py
@@ -6,6 +6,7 @@ app_name = 'authe'
 urlpatterns = [
     path('signin/', views.SignIn.as_view(),  name='signin'),
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signout/', views.signout, name='signout'),
     path('profile/', views.Profile.as_view(), name='profile'),
     
 ]
