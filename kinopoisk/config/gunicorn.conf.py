8b8c9fc01c80ea91abc08828f363e28f419b4b5e
unknown <a_saudabekov@kbtu.kz>
2024-01-16 16:04:56 +0300

    server side settings

--- kinopoisk/config/gunicorn.conf.py
+++ kinopoisk/config/gunicorn.conf.py
@@ -2,4 +2,4 @@ bind = '127.0.0.1:8000'
 workers = 2
 user = 'tebirenn'
 timeout = 120 # seconds
-wsgi_app = '/home/tebirenn/factory/backend/backend/wsgi:application'
+wsgi_app = '/home/tebirenn/kino_6/kinopoisk/kinopoisk/wsgi:application'
