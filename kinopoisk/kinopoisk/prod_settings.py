cb1341bc443a43ae76af7161ae26e1b66f962063
unknown <a_saudabekov@kbtu.kz>
2024-01-16 15:19:07 +0300

    server side settings

---
+++ kinopoisk/kinopoisk/prod_settings.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-+_(cr#3654v5d_j4!$hx+n0w75b7&7()0l*dsfvhjfdsvhj'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '159.89.4.57']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIR = [STATIC_DIR]
+
+# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- kinopoisk/kinopoisk/settings.py
+++ kinopoisk/kinopoisk/settings.py
@@ -1,22 +1,7 @@
-from pathlib import Path
-import os
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h0+97xfrtf&9$4)$&f4ztk$72mcb4=_b0m1exh_=0hpk#9%tf)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
+try:
+    from .local_settings import *
+except:
+    from .prod_settings import *
 
 # Application definition
 INSTALLED_APPS = [
@@ -32,7 +17,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'corsheaders',
-    # 'fontawesomefree',
     'authe',
     'fav',
     'movies',
@@ -94,17 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kinopoisk.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -138,16 +111,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIR = ['authe', 'fav', 'movies']
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -156,5 +119,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authe.User'
 
 CORS_ALLOWED_ORIGINS = [ 
-    "http://localhost:8000", 
+    'http://localhost:8000',
 ]
